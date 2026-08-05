53124fab0c534a33d0d37340ed6e98f5d5b5cbc3
Malentha-77 <mrt9920521@gmail.com>
2026-08-05 12:02:14 +0200

    added wasss

--- blackjack-actions/blackjack.py
+++ blackjack-actions/blackjack.py
@@ -1,8 +1,8 @@
-#commentttttt
 RANK_VALUES = {
     "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
-    "J": 10, "Q": 10, "K": 10, "A": 11,
+    "J": 10, "Q": 10, "K": 10, "A": 11, #just a comment
 }
+#wassss
 
 
 def hand_value(cards):
